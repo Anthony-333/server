65ad29e4af9e1963335c4fd6346e2af143933bb3
anthony333 <anthonyoling333@gmail.com>
2023-10-14 19:40:59 +0800

    update

--- src/Routes/UsersRoute.ts
+++ src/Routes/UsersRoute.ts
@@ -5,7 +5,7 @@ import requiredAuth from "../middleware/requiredAuth.ts";
 const router = express.Router();
 
 // Register Route
-router.use(requiredAuth);
+// router.use(requiredAuth);
 router.get("/allUsers", getAllUsers);
 
 export default router;
